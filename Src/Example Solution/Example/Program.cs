7f1b9f555171c5c9059e8e15fcf8d8c45cc07545
Daniel Porrey <porrd0bl@prscoad.com>
2020-01-25 10:01:04 -0600

    Added example.

---
+++ Src/Example Solution/Example/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Sample
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			using (SomeObject obj = new SomeObject())
+			{
+				// ***
+				// *** obj will be disposed and the dispose
+				// *** methods will be called.
+			}
+		}
+	}
+}
---
+++ Src/Example Solution/Example/SomeObject.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Sample
+{
+	public class SomeObject : DisposableObject
+	{
+		protected override void OnDisposeManagedObjects()
+		{
+			// ***
+			// *** Disposed CLR managed objects here.
+			// ***
+		}
+
+		protected override void OnDisposeUnmanagedObjects()
+		{
+			// ***
+			// *** Disposed non-CLR managed objects here.
+			// ***
+		}
+	}
+}
